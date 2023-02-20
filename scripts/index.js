6823b182feb050bb959c0672cdebc2dcb1a15eb6
Jonathan Brandt <jonathan.brandt1@gmail.com>
2023-02-19 21:31:49 -0800

    New js file

---
+++ scripts/index.js
@@ -0,0 +1,26 @@
+let initialCards = [
+  {
+    name: "Mount Rushmore",
+    link: "https://images.unsplash.com/photo-1572925192237-6ff8e0599888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80",
+  },
+  {
+    name: "Golden Gate Bridge",
+    link: "https://images.unsplash.com/photo-1610312278520-bcc893a3ff1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=847&q=80",
+  },
+  {
+    name: "Yosemite Valley",
+    link: "https://images.unsplash.com/photo-1572787514629-72872e4fc123?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "Devil's Bridge",
+    link: "https://images.unsplash.com/photo-1588272947905-24bec36c176d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "Tolmie Peak",
+    link: "https://images.unsplash.com/photo-1567964183446-cfd9803becac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+  },
+  {
+    name: "Laguna Beach",
+    link: "https://images.unsplash.com/photo-1625029117283-7a9f7602a134?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=507&q=80",
+  },
+];

9ae421d1ba66b1172735d84dab7fdd9052560696
Jonathan Brandt <jonathan.brandt1@gmail.com>
2023-02-20 05:14:48 -0800

    project 4 stage 2

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,26 @@
-let initialCards = [
-  {
-    name: "Mount Rushmore",
-    link: "https://images.unsplash.com/photo-1572925192237-6ff8e0599888?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1965&q=80",
-  },
-  {
-    name: "Golden Gate Bridge",
-    link: "https://images.unsplash.com/photo-1610312278520-bcc893a3ff1d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=847&q=80",
-  },
+const initialCards = [
   {
     name: "Yosemite Valley",
-    link: "https://images.unsplash.com/photo-1572787514629-72872e4fc123?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
   },
   {
-    name: "Devil's Bridge",
-    link: "https://images.unsplash.com/photo-1588272947905-24bec36c176d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
   },
   {
-    name: "Tolmie Peak",
-    link: "https://images.unsplash.com/photo-1567964183446-cfd9803becac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
   },
   {
-    name: "Laguna Beach",
-    link: "https://images.unsplash.com/photo-1625029117283-7a9f7602a134?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=507&q=80",
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
   },
 ];

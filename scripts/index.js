a4e048d4cf52da24d66e07948d7b089dd6d13a53
Jonathan Brandt <jonathan.brandt1@gmail.com>
2023-03-03 00:49:45 -0800

    project 4 complete

--- scripts/index.js
+++ scripts/index.js
@@ -25,16 +25,66 @@ const initialCards = [
   },
 ];
 
-const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
+const profileEditButton = document.querySelector(".profile__edit-button");
 const profileEditCloseButton = profileEditModal.querySelector(
   "#profile-edit-close-button"
 );
+const profileForm = document.querySelector("#profile-modal-form");
+const profileTitle = document.querySelector(".profile__title");
+const profileDescription = document.querySelector(".profile__description");
+const titleInput = profileEditModal.querySelector("#modal-input-title");
+const descriptionInput = profileEditModal.querySelector(
+  "#modal-input-description"
+);
+const galleryCards = document.querySelector(".gallery__cards");
 
-profileEditButton.addEventListener("click", () => {
+function getCardElement(data) {
+  const cardTemplate = document
+    .querySelector("#card-template")
+    .content.querySelector(".card__element");
+  const cardElement = cardTemplate.cloneNode(true);
+
+  const cardImage = cardTemplate.querySelector(".card__image");
+  const cardTitle = cardTemplate.querySelector(".card__title");
+
+  cardImage.src = data.link;
+  cardImage.alt = data.name;
+  cardTitle.textContent = data.name;
+
+  return cardElement;
+}
+
+function addCard(data) {
+  galleryCards.append(getCardElement(data));
+}
+
+function openModal() {
+  titleInput.value = profileTitle.textContent;
+  descriptionInput.value = profileDescription.textContent;
+
   profileEditModal.classList.add("modal__opened");
-});
+}
 
-profileEditCloseButton.addEventListener("click", () => {
+function closeModal() {
   profileEditModal.classList.remove("modal__opened");
-});
+}
+
+function handleProfileFormSubmit(evt) {
+  evt.preventDefault();
+
+  profileTitle.textContent = titleInput.value;
+  profileDescription.textContent = descriptionInput.value;
+
+  closeModal();
+}
+
+for (let i = 0; i < initialCards.length; i++) {
+  const data = initialCards[i];
+  getCardElement(data);
+  addCard(data);
+}
+
+profileForm.addEventListener("submit", handleProfileFormSubmit);
+profileEditButton.addEventListener("click", openModal);
+profileEditCloseButton.addEventListener("click", closeModal);
